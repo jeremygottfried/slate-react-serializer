b092641811152f762dee310876104f0570d549ca
Jeremy Gottfried <jeremyagottfried@gmail.com>
2021-04-08 17:58:49 -0400

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'slate-react-serializer'
+import 'slate-react-serializer/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
